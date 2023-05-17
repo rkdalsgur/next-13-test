e97a022ccfaa9023e0c45d07265a320d55133f63
rkdalsgur <127273488+rkdalsgur@users.noreply.github.com>
2023-05-17 12:41:59 +0900

    '1st'

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="mt-10 text-2xl font-bold">About page</h1>
+      <p>자기소개</p>
+    </div>
+  )
+}
---
+++ app/project/page.tsx
@@ -0,0 +1,8 @@
+export default function Projext() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="mt-10 text-2xl font-bold">Project</h1>
+      <p>프로젝트 소개</p>
+    </div>
+  )
+}
